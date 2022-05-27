a6245dc033f5d4fbb4ff45fb4513603eb979fb08
Wackar <wackar.afzal@gmail.com>
2022-05-27 08:42:44 +0500

    ready to be deployed

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -294,22 +294,7 @@ const Ecommerce = () => {
             <p className="text-gray-400 text-sm">36 Recent Transactions</p>
           </div>
         </div>
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Daily Activities</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50 "
-              src={product9}
-              alt=""
-            />
-         
-          </div>
-        </div>
+       
       </div>
     </div>
   );
